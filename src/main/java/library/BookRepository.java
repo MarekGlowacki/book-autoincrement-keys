9a934f11c1c93db454eae478790bb64a946f46df
User <user@its.me>
2023-01-05 08:53:02 +0100

    Autogenerate ID by Spring

---
+++ src/main/java/library/BookRepository.java
@@ -0,0 +1,5 @@
+package library;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookRepository extends CrudRepository<Book, Long> { }
---
+++ src/main/java/library/LibraryApplication.java
@@ -0,0 +1,19 @@
+package library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(LibraryApplication.class, args);
+        BookRepository bookRepository = context.getBean(BookRepository.class);
+        Book book1 = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz", "Greg", 312, 2002);
+        bookRepository.save(book1);
+        Book book2 = new Book("Tytul", "autor", "publikator", 777, 2023);
+        bookRepository.save(book2);
+    }
+
+}
